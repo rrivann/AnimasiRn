d30489ca22e18c4afc5ff7551b3deee9ab8bb79e
Rivan Albani Ray <72599848+rrivann@users.noreply.github.com>
2020-11-19 10:08:31 +0700

    done Anim2

--- src/Page/Anim2/index.js
+++ src/Page/Anim2/index.js
@@ -1,42 +1,151 @@
 import React from 'react';
-import {StyleSheet, Text, View, Dimensions} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  StatusBar,
+  Animated,
+} from 'react-native';
+import dayjs from 'dayjs';
 
 const {width} = Dimensions.get('screen');
 const SIZE = width * 0.9;
+const TICK_INTERVAL = 1000;
 
-const index = () => {
-  const rotateSeconds = '25deg';
-  const transformSecond = {
-    transform: [{rotate: rotateSeconds}],
+export default class index extends React.Component {
+  state = {
+    index: new Animated.Value(0),
+    tick: new Animated.Value(0),
+    scale: [...Array(6).keys()].map(() => new Animated.Value(0)),
   };
-  const rotateMinutes = '125deg';
-  const transformMinutes = {
-    transform: [{rotate: rotateMinutes}],
-  };
-  const rotateHours = '65deg';
-  const transformHours = {
-    transform: [{rotate: rotateHours}],
-  };
-  return (
-    <View style={styles.page}>
-      <View style={styles.bigQuadran} />
-      <View style={styles.mediumQuadran} />
-      <View style={styles.smallQuadran} />
-      <View style={[styles.mover, transformHours]}>
-        <View style={styles.hours}></View>
-      </View>
-      <View style={[styles.mover, transformMinutes]}>
-        <View style={styles.minutes}></View>
-      </View>
-      <View style={[styles.mover, transformSecond]}>
-        <View style={styles.seconds}></View>
-      </View>
-    </View>
-  );
-};
 
-export default index;
+  _timer = 0;
+  _ticker = null;
 
+  componentDidMount() {
+    const current = dayjs();
+    const diff = current.endOf('day').diff(current, 'second');
+    const oneDay = 24 * 60 * 60;
+
+    console.log('scale: ', this.state.scale);
+
+    this._timer = oneDay - diff;
+    this.state.tick.setValue(this._timer);
+    this.state.index.setValue(this._timer - 30);
+
+    this._animate();
+
+    this._ticker = setInterval(() => {
+      this._timer += 1;
+      this.state.tick.setValue(this._timer);
+    }, TICK_INTERVAL);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this._ticker);
+    this._ticker = null;
+  }
+
+  _animate = () => {
+    const scaleStaggerAnimations = this.state.scale.map((animated) => {
+      return Animated.spring(animated, {
+        toValue: 1,
+        tension: 18,
+        friction: 3,
+        useNativeDriver: true,
+      });
+    });
+
+    Animated.parallel([
+      Animated.stagger(
+        TICK_INTERVAL / this.state.scale.length,
+        scaleStaggerAnimations,
+      ),
+      Animated.timing(this.state.index, {
+        toValue: this.state.tick,
+        duration: TICK_INTERVAL / 2,
+        useNativeDriver: true,
+      }),
+    ]).start();
+  };
+
+  render() {
+    const {
+      index,
+      scale: [
+        smallQuadranScale,
+        mediumQuadranScale,
+        bigQuadranScale,
+        secondsScale,
+        minutesScale,
+        hoursScale,
+      ],
+    } = this.state;
+
+    const interpolated = {
+      inputRange: [0, 360],
+      outputRange: ['0deg', '360deg'],
+    };
+
+    const secondDegrees = Animated.multiply(index, 6);
+    const transformSecond = {
+      transform: [
+        {rotate: secondDegrees.interpolate(interpolated)},
+        {scale: secondsScale},
+      ],
+    };
+
+    const rotateMinutes = Animated.divide(
+      secondDegrees,
+      new Animated.Value(60),
+    );
+    const transformMinutes = {
+      transform: [
+        {rotate: rotateMinutes.interpolate(interpolated)},
+        {scale: minutesScale},
+      ],
+    };
+
+    const rotateHours = Animated.divide(rotateMinutes, new Animated.Value(12));
+    const transformHours = {
+      transform: [
+        {rotate: rotateHours.interpolate(interpolated)},
+        {scale: hoursScale},
+      ],
+    };
+
+    return (
+      <View style={styles.page}>
+        <StatusBar hidden={true} />
+        <Animated.View
+          style={[styles.bigQuadran, {transform: [{scale: bigQuadranScale}]}]}
+        />
+        <Animated.View
+          style={[
+            styles.mediumQuadran,
+            {transform: [{scale: mediumQuadranScale}]},
+          ]}
+        />
+        <Animated.View style={[styles.mover, transformHours]}>
+          <View style={styles.hours}></View>
+        </Animated.View>
+        <Animated.View style={[styles.mover, transformMinutes]}>
+          <View style={styles.minutes}></View>
+        </Animated.View>
+        <Animated.View style={[styles.mover, transformSecond]}>
+          <View style={styles.seconds}></View>
+        </Animated.View>
+        <Animated.View
+          style={[
+            styles.smallQuadran,
+            {transform: [{scale: smallQuadranScale}]},
+          ]}
+        />
+      </View>
+    );
+  }
+}
 const styles = StyleSheet.create({
   page: {
     flex: 1,
@@ -53,7 +162,6 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
   },
   hours: {
-    position: 'absolute',
     backgroundColor: 'rgba(0, 0, 0, 0.4)',
     height: '35%',
     marginTop: '15%',
@@ -61,7 +169,6 @@ const styles = StyleSheet.create({
     borderRadius: 4,
   },
   minutes: {
-    position: 'absolute',
     backgroundColor: 'rgba(0, 0, 0, 0.8)',
     height: '45%',
     marginTop: '5%',
@@ -69,7 +176,6 @@ const styles = StyleSheet.create({
     borderRadius: 3,
   },
   seconds: {
-    position: 'absolute',
     backgroundColor: 'rgba(227, 71, 134, 1)',
     height: '50%',
     width: 2,
