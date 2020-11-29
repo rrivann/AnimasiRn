e2202d875c64f73e99cf4e0efc7a9f5d2223270d
Rivan Albani Ray <72599848+rrivann@users.noreply.github.com>
2020-11-29 18:52:47 +0700

    update Anim8

--- src/Page/index.js
+++ src/Page/index.js
@@ -6,5 +6,6 @@ import Anim4 from './Anim4';
 import Anim5 from './Anim5';
 import Anim6 from './Anim6';
 import Anim7 from './Anim7';
+import Anim8 from './Anim8';
 
-export {Home, Anim1, Anim2, Anim3, Anim4, Anim5, Anim6, Anim7};
+export {Home, Anim1, Anim2, Anim3, Anim4, Anim5, Anim6, Anim7, Anim8};
