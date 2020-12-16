dda19c2e540aef3c451d7a3dadc53ab9fbb9e8b9
Rivan Albani Ray <72599848+rrivann@users.noreply.github.com>
2020-12-17 06:26:18 +0700

    update Anim 15

--- src/Page/Anim15/index.js
+++ src/Page/Anim15/index.js
@@ -1,14 +1,74 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-
-const index = () => {
-  return (
-    <View>
-      <Text></Text>
-    </View>
-  );
+import {
+  Vibration,
+  StatusBar,
+  Easing,
+  TextInput,
+  Dimensions,
+  Animated,
+  TouchableOpacity,
+  FlatList,
+  Text,
+  View,
+  StyleSheet,
+} from 'react-native';
+const {width, height} = Dimensions.get('window');
+const colors = {
+  black: '#323F4E',
+  red: '#F76A6A',
+  text: '#ffffff',
 };
 
-export default index;
+const timers = [...Array(13).keys()].map((i) => (i === 0 ? 1 : i * 5));
+const ITEM_SIZE = width * 0.38;
+const ITEM_SPACING = (width - ITEM_SIZE) / 2;
 
-const styles = StyleSheet.create({});
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar hidden />
+      <Animated.View
+        style={[
+          StyleSheet.absoluteFillObject,
+          {
+            justifyContent: 'flex-end',
+            alignItems: 'center',
+            paddingBottom: 100,
+          },
+        ]}>
+        <TouchableOpacity onPress={() => {}}>
+          <View style={styles.roundButton} />
+        </TouchableOpacity>
+      </Animated.View>
+      <View
+        style={{
+          position: 'absolute',
+          top: height / 3,
+          left: 0,
+          right: 0,
+          flex: 1,
+        }}>
+        <Text style={styles.text}>1</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.black,
+  },
+  roundButton: {
+    width: 80,
+    height: 80,
+    borderRadius: 80,
+    backgroundColor: colors.red,
+  },
+  text: {
+    fontSize: ITEM_SIZE * 0.8,
+    fontFamily: 'Menlo',
+    color: colors.text,
+    fontWeight: '900',
+  },
+});
